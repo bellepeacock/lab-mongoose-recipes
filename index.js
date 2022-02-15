f915e103b15bd9d178315a2f666eb3c0e95bdb0e
Annabelle Peacock <aspeacock92@gmail.com>
2022-02-15 21:44:06 +0100

    levels throwing errors, iteration 2 kinda done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+console.log(data.map(item => item.title));
 
 const express = require('express');
 
@@ -16,21 +17,35 @@ mongoose
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    const createRecipe = (title, level, ingredients, cuisine, dishType, image, duration, creator, created) => {
-      const newRecipe = new Recipe({ title, level, ingredients, cuisine, dishType, image, duration, creator, created })
-      console.log(newRecipe.title)
-
-      newRecipe.save()
-      .then(newlyCreatedRecipe => console.log(newlyCreatedRecipe))
-      .catch(error => console.log(error));
-    };
-    
-    createRecipe('Gordon Bleu', 'amateur_chef', ['tomato', 'carrot'], 'french', 'breakfast', 'image_url', 5, 'Gordon Ramsay')
+    // return Recipe.deleteMany()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
 });
 
+
+const createRecipe = (title, level, ingredients, cuisine, dishType, image, duration, creator, created) => {
+  const newRecipe = new Recipe({
+    title,
+    level: level.split(" ").join("_"),
+    ingredients,
+    cuisine,
+    dishType: dishType.split(" ").join("_"),
+    image,
+    duration,
+    creator,
+    created
+  })
+  console.log(newRecipe.title)
+  newRecipe.save()
+  .then(newlyCreatedRecipe => console.log(newlyCreatedRecipe))
+  .catch(error => console.log(error));
+};
+
+
+// createRecipe('Gordon Bleu', 'amateur_chef', ['tomato', 'carrot'], 'french', 'breakfast', 'image_url', 5, 'Gordon Ramsay')
+
+
+Recipe.insertMany(data)
+  .then(createdRecipes => console.log(createdRecipes))
+  .catch(error => console.log(error))
