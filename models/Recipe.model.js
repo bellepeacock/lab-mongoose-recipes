3e8e49547b808bbec1a4e2694452ce93fe50d76d
Annabelle Peacock <aspeacock92@gmail.com>
2022-02-16 14:14:44 +0100

    error with keys

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   title: String,
-  level: {type: String, enum: ["amateur_chef", "easy_peasy", "ultrapro_chef"]},
+  level: {type: String, enum: ["Amateur Chef", "Easy Peasy", "Ultrapro Chef"]},
   ingredients: [ String ],
   cuisine: { type: String, isRequired: true },
   dishType: {type: String, enum: ["breakfast", "main_course", "soup", "snack", "drink", "desert", "other"]},
